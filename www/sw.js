7cfcbe891f8b9f6846b50ea8391195c14d02342c
joazco <jazoulay@joazco.com>
2023-04-03 10:02:59 +0200

    update sw.js

--- www/sw.js
+++ www/sw.js
@@ -1,10 +1,26 @@
 /** VARS */
-const CACHE_NAME = "offline";
+const ORIGIN_URL = `${location.protocol}//${location.host}`;
+const CACHE_NAME = "offline-v1";
 const OFFLINE_URL = "offline.html";
-
+const CACHED_FILES = [
+  OFFLINE_URL,
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js",
+  `${ORIGIN_URL}/css/index.css`,
+  `${ORIGIN_URL}/js/index.js`,
+  `${ORIGIN_URL}/img/logo.png`,
+];
 /** FUNCTIONS */
 
 /** Fetch */
+
+const sendOfflinePage = (resolve) => {
+  caches.open(CACHE_NAME).then((cache) => {
+    cache.match(OFFLINE_URL).then((cachedResponse) => {
+      resolve(cachedResponse);
+    });
+  });
+};
 
 const respondWithFetchPromiseNavigate = (event) =>
   new Promise((resolve) => {
@@ -20,21 +36,9 @@ const respondWithFetchPromiseNavigate = (event) =>
             resolve(networkResponse);
           })
           // send cache offline.html
-          .catch(() => {
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.match(OFFLINE_URL).then((cachedResponse) => {
-                resolve(cachedResponse);
-              });
-            });
-          });
+          .catch(() => sendOfflinePage(resolve));
       })
-      .catch(() => {
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.match(OFFLINE_URL).then((cachedResponse) => {
-            resolve(cachedResponse);
-          });
-        });
-      });
+      .catch(() => sendOfflinePage(resolve));
   });
 
 const fetchSW = (event) => {
@@ -42,20 +46,36 @@ const fetchSW = (event) => {
   // for an HTML page.
   if (event.request.mode === "navigate") {
     event.respondWith(respondWithFetchPromiseNavigate(event));
+  } else if (CACHED_FILES.includes(event.request.url)) {
+    event.respondWith(caches.match(event.request));
   }
 };
 
 /*********************************** */
 
 /** Activate */
+const deleteOldCaches = () =>
+  new Promise((resolve) => {
+    caches.keys().then((keys) => {
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            caches.delete(key);
+          }
+        })
+      ).finally(resolve);
+    });
+  });
 
 const waitUntilActivatePromise = () =>
   new Promise((resolve) => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ("navigationPreload" in self.registration) {
-      self.registration.navigationPreload.enable().finally(resolve);
-    }
+    deleteOldCaches().then(() => {
+      // Enable navigation preload if it's supported.
+      // See https://developers.google.com/web/updates/2017/02/navigation-preload
+      if ("navigationPreload" in self.registration) {
+        self.registration.navigationPreload.enable().finally(resolve);
+      }
+    });
   });
 
 const activate = (event) => {
@@ -70,7 +90,7 @@ const activate = (event) => {
 const waitUntilInstallationPromise = () =>
   new Promise((resolve) => {
     caches.open(CACHE_NAME).then((cache) => {
-      cache.add(new Request(OFFLINE_URL, { cache: "reload" })).then(resolve);
+      cache.addAll(CACHED_FILES).then(resolve);
     });
   });
 
